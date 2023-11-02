9eea20e8246138cabe7e81fc9177900691044baf
joshua ogunwole <joshuaogunwolejo888@gmail.com>
2023-11-02 01:06:04 +0100

    create a new database controller

---
+++ src/database/index.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import config from 'config'
+
+class Database{
+    // constructor(){
+    //   const {host} = config.get('dbConfig')
+    //     mongoose.connect(host)
+    //     .then(() => {
+    //       console.log('Database connection successful')
+    //     })
+    //     .catch(err => {
+    //       console.error('Database connection error')
+    //       console.log('err',err)
+    //     })
+    // }
+}
+
+export default Database;
